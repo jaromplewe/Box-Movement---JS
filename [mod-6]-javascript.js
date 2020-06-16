cc42baa3ad27d4b66fe8404001f1cf32e8b7870e
jaromplewe <66282922+jaromplewe@users.noreply.github.com>
2020-06-16 11:50:58 -0600

    Change the box - JS

---
+++ [mod-6]-javascript.js
@@ -0,0 +1,27 @@
+function growBox() {
+  document.getElementById("box").className = "grow-box";
+}
+
+function blueBox() {
+    document.getElementById("box").className = "blue-box";
+  }
+
+function fadeBox() {
+    document.getElementById("box").className = "fade-box";
+  }
+
+function resetBox() {
+    document.getElementById("box").className = "normal-box";
+  }
+
+function triangle() {
+    document.getElementById("box").className = "triangle";
+  }
+
+function circle() {
+    document.getElementById("box").className = "circle";
+  }
+
+function shake() {
+    document.getElementById("box").className = "shake-box";
+  }
